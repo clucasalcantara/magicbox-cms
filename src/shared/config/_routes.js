ba4a3a0493a6b8cb5a1d4490258cd29000c1260e
Caio <alcantaracaiolucas@gmail.com>
2018-03-19 14:37:18 +0000

    feat(autho0-login): Add initial authService
    feat(autho0-login): Add initial authService

--- src/shared/config/_routes.js
+++ src/shared/config/_routes.js
@@ -9,12 +9,19 @@ import { Route } from 'react-router-dom'
 import { MATCH_ROUTE_EXACT } from './constants'
 // Import containers components
 import Home from '../../client/components/pages/Home'
+import Login from '../../client/components/pages/Login'
 
 
 export default [
   {
     path: '/',
     component: Home,
-    ...MATCH_ROUTE_EXACT
+    ...MATCH_ROUTE_EXACT,
   },
+  {
+    path: '/login',
+    component: 'Login',
+    ...MATCH_ROUTE_EXACT,
+  }
 ]
+
