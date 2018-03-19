173bf64d0fd29df5acad022907ad9ec2e9be1327
Caio <alcantaracaiolucas@gmail.com>
2018-03-19 10:51:12 +0000

    feat(engines): Add engine initial thought

---
+++ src/shared/engines/index.js
@@ -0,0 +1,20 @@
+/**
+ * Entrypoint for magicbox engines
+ * @memberOf magicbox-cms
+ *
+ **/
+import * as accessEngine from './access'
+import * as deployEngine from './deploy'
+import * as garbageEngine from './garbage'
+import * as layoutEngine from './layout'
+import * as pageEngine from './pages'
+import * as themeEngine from './themes'
+
+module.exports = {
+  accesEngine,
+  deployEngine,
+  garbageEngine,
+  layoutEngine,
+  pageEngine,
+  themeEngine,
+}
